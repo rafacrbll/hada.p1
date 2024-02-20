e9fc2c56b54f950809d35b16cf12e963afc14819
rafacrbll <142307900+rafacrbll@users.noreply.github.com>
2024-02-20 12:05:37 +0100

    Documentado el código de hadaP1

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    //Esta clase controla los métodos de tiempo en segundos y minutos
     class HadaP1
     {
+        //Este método cambia de segundos a minutos
         public static double Seconds2Minutes(double s)
         {
             if (s == 0)
@@ -17,6 +19,7 @@ namespace hada_p1
             return s / 60;
         }
 
+        //Este método cambia de minutos a segundos
         public static double Minutes2Seconds(double m)
         {
             if (m == 0)
